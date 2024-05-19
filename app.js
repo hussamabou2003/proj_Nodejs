e1b864118bb2c3f8a75d1996b2204bc3948d6b5c
hussamabou2003 <hussamabushanab48@gmail.com>
2024-05-19 20:55:02 +0300

    hussamabou2003

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const { check, validationResult } = require('express-validator');
 // إعداد التطبيق
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static('public'));
 app.set('views', './views')
 app.set('view engine', 'pug')
 
@@ -101,6 +102,78 @@ app.get('/view', [
     });
 });
 
+app.get('/students/:id', (req, res) => {
+
+    let id = req.params.id;
+    let sql = `SELECT * FROM students WHERE id = ?`;
+    db.get(sql, id, (err, row) => {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+        if (row == undefined) {
+            res.status(404).send("Student not found with id " + id);
+        } else {
+            //res.json(row);
+            res.render('student', { student: row });
+        }
+    });
+});
+// الرابط لعرض صفحة الطالب
+app.get('/students/:id', (req, res) => {
+    let id = req.params.id;
+    let sql = `SELECT * FROM students WHERE id = ?`;
+    db.get(sql, id, (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        if (row == undefined) {
+            res.status(404).send("Student not found with id " + id);
+        } else {
+            res.render('student', { student: row });
+        }
+    });
+});
+
+// الرابط لتعديل معلومات الطالب من صفحة الطالب
+app.post('/students/:id/edit', [
+    check('name').isLength({ min: 1 }),
+    check('age').isNumeric(),
+    check('id').isNumeric(),
+    check('score').isNumeric()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    let data = [req.body.name, req.body.age, req.body.score, req.body.id];
+    let sql = `UPDATE students SET name = ?, age = ?, score = ? WHERE id = ?`;
+    db.run(sql, data, function(err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.redirect('/students/' + req.body.id);
+    });
+});
+
+// الرابط لحذف طالب من صفحة الطالب
+app.post('/students/:id/delete', [
+    check('id').isNumeric()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    let id = req.body.id;
+    let sql = `DELETE FROM students WHERE id = ?`;
+    db.run(sql, id, function(err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.redirect('/');
+    });
+});
+
+
 // الرابط لصفحة الويب الرئيسية
 app.get('/', (req, res) => {
     let sql = `SELECT * FROM students`;
@@ -108,11 +181,318 @@ app.get('/', (req, res) => {
         if (err) {
             return res.status(500).send(err.message);
         }
-        res.render('profile', { students: row });
+        res.render('students', { students: row });
     });
 });
+
+// الرابط لعرض صفحة الطالب
+app.get('/students/:id', (req, res) => {
+    let id = req.params.id;
+    let sql = `SELECT * FROM students WHERE id = ?`;
+    db.get(sql, id, (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        if (row == undefined) {
+            res.status(404).send("Student not found with id " + id);
+        } else {
+            res.render('student', { student: row });
+        }
+    });
+});
+
+// الرابط لتعديل معلومات الطالب من صفحة الطالب
+app.post('/students/:id/edit', [
+    check('name').isLength({ min: 1 }),
+    check('age').isNumeric(),
+    check('id').isNumeric(),
+    check('score').isNumeric()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    let data = [req.body.name, req.body.age, req.body.score, req.body.id];
+    let sql = `UPDATE students SET name = ?, age = ?, score = ? WHERE id = ?`;
+    db.run(sql, data, function(err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.redirect('/students/' + req.body.id);
+    });
+});
+
+// الرابط لحذف طالب من صفحة الطالب
+app.post('/students/:id/delete', [
+    check('id').isNumeric()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    let id = req.body.id;
+    let sql = `DELETE FROM students WHERE id = ?`;
+    db.run(sql, id, function(err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.redirect('/');
+    });
+});
+
+
 
 // بدء التطبيق
 app.listen(3000, () => {
     console.log('App is running on port 3000');
 });
+
+
+
+// const express = require('express');
+// const sqlite3 = require('sqlite3').verbose();
+// const bodyParser = require('body-parser');
+// const path = require('path');
+// const { check, validationResult } = require('express-validator');
+
+// // إعداد التطبيق
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// //app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/static', express.static('public'));
+// app.set('views', './views')
+// app.set('view engine', 'pug')
+
+// // إعداد قاعدة البيانات
+// let db = new sqlite3.Database(__dirname + '\\studentsdb.db', (err) => {
+//     if (err) {
+//         console.error(err.message);
+//     }
+//     console.log('Connected to the students database.');
+// });
+
+// // الرابط لإضافة طالب جديد
+// app.post('/add', [
+//     check('name').isLength({ min: 1 }),
+//     check('age').isNumeric(),
+//     check('id').isNumeric(),
+//     check('score').isNumeric()
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+//     let data = [req.body.name, req.body.age, req.body.id, req.body.score];
+//     let sql = `INSERT INTO students (name, age, id, score) VALUES(?, ?, ?, ?)`;
+//     db.run(sql, data, function(err) {
+//         if (err) {
+//             return res.status(500).send(err.message);
+//         }
+//         res.send(`A row has been inserted with rowid ${this.lastID}`);
+//     });
+// });
+
+// // الرابط لتعديل معلومات الطالب
+// app.post('/edit', [
+//     check('name').isLength({ min: 1 }),
+//     check('age').isNumeric(),
+//     check('id').isNumeric(),
+//     check('score').isNumeric()
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+//     let data = [req.body.name, req.body.age, req.body.score, req.body.id];
+//     let sql = `UPDATE students SET name = ?, age = ?, score = ? WHERE id = ?`;
+//     db.run(sql, data, function(err) {
+//         if (err) {
+//             return res.status(500).send(err.message);
+//         }
+//         res.send(`Row(s) updated: ${this.changes}`);
+//     });
+// });
+
+// // الرابط لحذف طالب
+// app.post('/delete', [
+//     check('id').isNumeric()
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+//     let id = req.body.id;
+//     let sql = `DELETE FROM students WHERE id = ?`;
+//     db.run(sql, id, function(err) {
+//         if (err) {
+//             return res.status(500).send(err.message);
+//         }
+//         res.send(`Row(s) deleted ${this.changes}`);
+//     });
+// });
+
+// // الرابط لعرض معلومات الطالب
+// app.get('/view', [
+//     check('id').isNumeric()
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+//     let id = req.query.id;
+//     let sql = `SELECT * FROM students WHERE id = ?`;
+//     db.get(sql, id, (err, row) => {
+//         if (err) {
+//             return res.status(500).send(err.message);
+//         }
+//         if (row == undefined) {
+//             res.status(404).send("Student not found");
+//         } else {
+//             res.json(row);
+//         }
+//     });
+// });
+
+// app.get('/students/:id', (req, res) => {
+
+//     let id = req.params.id;
+//     let sql = `SELECT * FROM students WHERE id = ?`;
+//     db.get(sql, id, (err, row) => {
+//         if (err) {
+//             return res.status(500).send(err.message);
+//         }
+//         if (row == undefined) {
+//             res.status(404).send("Student not found with id " + id);
+//         } else {
+//             //res.json(row);
+//             res.render('student', { student: row });
+//         }
+//     });
+// });
+// // الرابط لعرض صفحة الطالب
+// app.get('/students/:id', (req, res) => {
+//     let id = req.params.id;
+//     let sql = `SELECT * FROM students WHERE id = ?`;
+//     db.get(sql, id, (err, row) => {
+//         if (err) {
+//             return console.error(err.message);
+//         }
+//         if (row == undefined) {
+//             res.status(404).send("Student not found with id " + id);
+//         } else {
+//             res.render('student', { student: row });
+//         }
+//     });
+// });
+
+// // الرابط لتعديل معلومات الطالب من صفحة الطالب
+// app.post('/students/:id/edit', [
+//     check('name').isLength({ min: 1 }),
+//     check('age').isNumeric(),
+//     check('id').isNumeric(),
+//     check('score').isNumeric()
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+//     let data = [req.body.name, req.body.age, req.body.score, req.body.id];
+//     let sql = `UPDATE students SET name = ?, age = ?, score = ? WHERE id = ?`;
+//     db.run(sql, data, function(err) {
+//         if (err) {
+//             return console.error(err.message);
+//         }
+//         res.redirect('/students/' + req.body.id);
+//     });
+// });
+
+// // الرابط لحذف طالب من صفحة الطالب
+// app.post('/students/:id/delete', [
+//     check('id').isNumeric()
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+//     let id = req.body.id;
+//     let sql = `DELETE FROM students WHERE id = ?`;
+//     db.run(sql, id, function(err) {
+//         if (err) {
+//             return console.error(err.message);
+//         }
+//         res.redirect('/');
+//     });
+// });
+
+
+// // الرابط لعرض صفحة الطالب
+// app.get('/students/:id', (req, res) => {
+//     let id = req.params.id;
+//     let sql = `SELECT * FROM students WHERE id = ?`;
+//     db.get(sql, id, (err, row) => {
+//         if (err) {
+//             return console.error(err.message);
+//         }
+//         if (row == undefined) {
+//             res.status(404).send("Student not found with id " + id);
+//         } else {
+//             res.render('student', { student: row });
+//         }
+//     });
+// });
+
+// // الرابط لتعديل معلومات الطالب من صفحة الطالب
+// app.post('/students/:id/edit', [
+//     check('name').isLength({ min: 1 }),
+//     check('age').isNumeric(),
+//     check('id').isNumeric(),
+//     check('score').isNumeric()
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+//     let data = [req.body.name, req.body.age, req.body.score, req.body.id];
+//     let sql = `UPDATE students SET name = ?, age = ?, score = ? WHERE id = ?`;
+//     db.run(sql, data, function(err) {
+//         if (err) {
+//             return console.error(err.message);
+//         }
+//         res.redirect('/students/' + req.body.id);
+//     });
+// });
+
+// // الرابط لحذف طالب من صفحة الطالب
+// app.post('/students/:id/delete', [
+//     check('id').isNumeric()
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+//     let id = req.body.id;
+//     let sql = `DELETE FROM students WHERE id = ?`;
+//     db.run(sql, id, function(err) {
+//         if (err) {
+//             return console.error(err.message);
+//         }
+//         res.redirect('/');
+//     });
+// });
+
+
+// // الرابط لصفحة الويب الرئيسية
+// app.get('/', (req, res) => {
+//     let sql = `SELECT * FROM students`;
+//     db.all(sql, (err, row) => {
+//         if (err) {
+//             return res.status(500).send(err.message);
+//         }
+//         res.render('students', { students: row });
+//     });
+// });
+
+// // بدء التطبيق
+// app.listen(3000, () => {
+//     console.log('App is running on port 3000');
+// });
